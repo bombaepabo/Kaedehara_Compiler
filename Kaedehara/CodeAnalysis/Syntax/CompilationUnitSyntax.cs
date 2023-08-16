5b231578ba18c1630231901343531bac28b656e0
phakkhapon aksonchanya <55224271+bombaepabo@users.noreply.github.com>
2023-08-16 14:26:02 +0700

    compilation unit

--- Kaedehara/CodeAnalysis/Syntax/CompilationUnitSyntax.cs
+++ Kaedehara/CodeAnalysis/Syntax/CompilationUnitSyntax.cs
@@ -1,14 +1,15 @@
 namespace Kaedehara.CodeAnalysis.Syntax
-
-    public sealed class CompilationUnitSyntax : SyntaxNode
 {
-    public CompilationUnitSyntax(ExpressionSyntax expression, SyntaxToken endofFileToken)
+    public sealed class CompilationUnitSyntax : SyntaxNode
     {
-        ExpressionSyntax = expression;
-        EndofFileToken = endofFileToken;
-    }
-    public override SyntaxKind Kind => SyntaxKind.CompilationUnit;
-    public ExpressionSyntax Expression { get; }
-    public SyntaxToken EndofFileToken { get; }
+        public CompilationUnitSyntax(ExpressionSyntax expression, SyntaxToken endofFileToken)
+        {
+            Expression = expression;
+            EndofFileToken = endofFileToken;
+        }
+        public override SyntaxKind Kind => SyntaxKind.CompilationUnit;
+        public ExpressionSyntax Expression { get; }
+        public SyntaxToken EndofFileToken { get; }
 
+    }
 }
